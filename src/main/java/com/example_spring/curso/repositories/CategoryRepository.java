2e953b92104f46c0a819feeeb9b4995bff83267f
Mendelsson Brandao <mendelsson@gmail.com>
2025-01-27 06:22:15 -0300

    Category

---
+++ src/main/java/com/example_spring/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example_spring.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example_spring.curso.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
