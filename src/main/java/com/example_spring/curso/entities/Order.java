ae59f8ca3aa445e6aea1f927ceee934a9a773cc6
Mendelsson Brandao <mendelsson@gmail.com>
2025-01-29 22:20:25 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/example_spring/curso/entities/Order.java
+++ src/main/java/com/example_spring/curso/entities/Order.java
@@ -23,8 +23,7 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	
+
 	private Instant moment;
 	
 	private Integer orderStatus;
